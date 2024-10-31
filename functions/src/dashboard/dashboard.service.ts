b25a44d32b96336258fb8829c6d2e86e179a1e19
Yianni Moustakas <yianni.m13@gmail.com>
2024-10-31 18:13:40 +1100

    noice

--- functions/src/dashboard/dashboard.service.ts
+++ functions/src/dashboard/dashboard.service.ts
@@ -1,6 +1,60 @@
 import {db} from '..';
 import admin from 'firebase-admin';
 import serviceAccount from '../permissions.json';
+import {logger} from "firebase-functions";
+
+type MonthlySearch = {
+    year: number;
+    month: number;
+    search_volume: number;
+}
+
+type KeywordResult = {
+    keyword: string;
+    spell: null;
+    location_code: number;
+    language_code: string;
+    search_partners: boolean;
+    competition: 'HIGH' | null;
+    competition_index: number | null;
+    search_volume: number | null;
+    low_top_of_page_bid: number | null;
+    high_top_of_page_bid: number | null;
+    cpc: number | null;
+    monthly_searches: MonthlySearch[] | null;
+}
+
+type TaskData = {
+    api: string;
+    function: string;
+    se: string;
+    keywords: string[];
+    location_name: string;
+    language_name: string;
+}
+
+type Task = {
+    id: string;
+    status_code: number;
+    status_message: string;
+    time: string;
+    cost: number;
+    result_count: number;
+    path: string[];
+    data: TaskData;
+    result: KeywordResult[];
+}
+
+type DataForSEOResponse = {
+    version: string;
+    status_code: number;
+    status_message: string;
+    time: string;
+    cost: number;
+    tasks_count: number;
+    tasks_error: number;
+    tasks: Task[];
+}
 
 export const createDashboardService = async (body: {
     name: string;
@@ -62,88 +116,109 @@ export const createDashboardService = async (body: {
         // Fetch keyword data from Google Ads API and save to database
         // Create an array to store keyword references
         const keywordRefs = [];
-        const tagRefs = [];
-
-        for (const keyword of keywords) {
-            // Query for existing keyword
-            const keywordQuery = await db.collection('keywords')
-                .where('name', '==', keyword.Keyword)
-                .get();
-
-            let keywordRef;
-
-            if (keywordQuery.empty) {
-                // Create new keyword if it doesn't exist
-                keywordRef = db.collection('keywords').doc();
-            } else {
-                // Use existing keyword reference if it exists
-                keywordRef = keywordQuery.docs[0].ref;
-            }
-
-            keywordRefs.push(keywordRef);
-
+        const tagRefs: any[] = [];
+        const dashboardTagTitleAndNames = [];
+        try {
             // Fetch data from DataForSEO
-            try {
-                const response =
-                    await fetch('https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live', {
-                        method: 'POST',
-                        headers: {
-                            'Authorization': 'Basic '
-                                + Buffer
-                                    .from(
-                                        serviceAccount.dataforseo_login
-                                        + ':'
-                                        + serviceAccount.dataforseo_password
-                                    )
-                                    .toString('base64'),
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify([
-                            {
-                                "keyword": keyword.Keyword,
-                                "location_name": "Australia",
-                                "language_name": "English"
-                            }
-                        ])
-                    });
+            const response =
+                await fetch('https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': 'Basic '
+                            + Buffer
+                                .from(
+                                    serviceAccount.dataforseo_login
+                                    + ':'
+                                    + serviceAccount.dataforseo_password
+                                )
+                                .toString('base64'),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify([
+                        {
+                            "keywords": keywords.map(({Keyword}) => Keyword),
+                            "location_name": "Australia",
+                            "language_name": "English"
+                        }
+                    ])
+                });
 
-                const dataForSEOResult = await response.json();
+            const dataForSEOResponse: DataForSEOResponse = await response
+                .json();
+
+            const dataForSeoResult = dataForSEOResponse
+                .tasks[0]
+                .result;
+
+            for (const keyword of keywords) {
+                // Result from dataForSEO
+                const dataForSEO = dataForSeoResult.find((res) => res.keyword === keyword.Keyword);
+                // Query for existing keyword
+                const keywordQuery = await db.collection('keywords')
+                    .where('name', '==', keyword.Keyword)
+                    .get();
+
+                let keywordRef;
+
+                if (keywordQuery.empty) {
+                    // Create new keyword if it doesn't exist
+                    keywordRef = db.collection('keywords').doc();
+                } else {
+                    // Use existing keyword reference if it exists
+                    keywordRef = keywordQuery.docs[0].ref;
+                }
+
+                keywordRefs.push(keywordRef);
 
                 const keywordTagRefs = [];
 
                 for (const [key, val] of Object.entries(keyword)) {
                     if (tagCategories.includes(key)) {
                         // Create a new tag
+                        dashboardTagTitleAndNames.push(key + val);
 
                         // Query for existing tag
                         const tagQuery = await db.collection('tags')
                             .where('name', '==', val)
+                            .where('tagCategory', '==', key)
                             .get();
 
                         let tagRef;
 
-                        if (tagQuery.empty) {
-                            // Create new tag if it doesn't exist
-                            tagRef = db.collection('tags').doc();
-                        } else {
+                        const newTag = tagRefs.find(async (t) => {
+                            const tg = await t.get()
+
+                            // @ts-ignore
+                            return tg.data.name === val && tg.data.tagCategory === key
+                        });
+
+                        logger.info('BLAH BLAH')
+
+                        if (!tagQuery.empty) {
                             // Use existing tag reference if it exists
                             tagRef = tagQuery.docs[0].ref;
+                        } else {
+                            // Create new tag if it doesn't exist
+                            tagRef = db.collection('tags').doc();
+                            // Find the corresponding tag category
+                            const categoryRef = tagCategoryRefs
+                                .find(async ref => {
+                                    const thing = await ref.get();
+
+                                    return thing.data.name === key;
+                                });
+
+                            batch.set(tagRef, {
+                                name: val,
+                                tagCategoryRef: categoryRef,
+                                tagCategory: key,
+                            });
                         }
 
-                        // Find the corresponding tag category
-                        const categoryRef = tagCategoryRefs
-                            .find(async ref => {
-                                const thing = await ref.get();
+                        if (!newTag) {
+                            tagRefs.push(tagRef);
+                        }
 
-                                return thing.data.name === key;
-                            });
-
-                        batch.set(tagRef, {
-                            name: val,
-                            tagCategoryRef: categoryRef,
-                            tagCategory: key,
-                        });
-                        tagRefs.push(tagRef);
                         keywordTagRefs.push(tagRef);
                     }
                 }
@@ -152,20 +227,13 @@ export const createDashboardService = async (body: {
                 batch.set(keywordRef, {
                     name: keyword.Keyword,
                     tags: keywordTagRefs,
-                    dataForSEO: dataForSEOResult, // Add the DataForSEO data
+                    dataForSEO, // Add the DataForSEO data
                     lastUpdated: admin.firestore.FieldValue.serverTimestamp() // Add timestamp for tracking
                 }, {merge: true});
 
-            } catch (error) {
-                console.error(`Error fetching DataForSEO data for keyword ${keyword.Keyword}:`, error);
-                // Continue with the loop even if DataForSEO fetch fails
-                batch.set(keywordRef, {
-                    name: keyword.Keyword,
-                    tags: tagRefs,
-                    dataForSEOError: 'Failed to fetch data',
-                    lastUpdated: admin.firestore.FieldValue.serverTimestamp()
-                }, {merge: true});
             }
+        } catch (error) {
+            console.error(`Error fetching DataForSEO data for campaign ${name}:`, error);
         }
 
         // Update the dashboard with the list of keyword references
